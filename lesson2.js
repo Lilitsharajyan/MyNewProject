cb4a094cb212e52ba017c3ba92b05713116c3626
karen300879 <71089405+karen300879@users.noreply.github.com>
2020-09-14 20:14:11 +0400

    Add files via upload

---
+++ lesson2.js
@@ -0,0 +1,3 @@
+let C=parseInt(prompt("Celsius"));
+let F=C*1.8+32;
+alert("Farenhait value is:" + F);
---
+++ task_circle.js
@@ -0,0 +1,6 @@
+const PI=Math.PI;
+r = parseFloat(prompt("Input the radius of a circle"));
+let l = 2 * PI * r;
+let s = PI * r * r;
+console.log("The perimeter of the circle is: " + l);
+console.log("The area of the circle is: " + s);
